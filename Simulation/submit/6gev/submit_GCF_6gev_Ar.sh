b8a3161af5bf3b565cbba119889814bad84adb8e
Erin Seroka <erinseroka@gwu.edu>
2021-07-02 14:33:41 -0400

    Added simulation scripts for Ar-40 (target geometry needs updating)

---
+++ Simulation/submit/6gev/submit_GCF_6gev_Ar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash                                                                                  
+
+source /apps/root/6.18.04/bin/thisroot.csh 
+
+for i in {1..100}
+do
+  ../../GCF_Generator_Suite/programs/genQE/genQE 18 22 6. ../../rootfiles/ar_6gev_$i.root 50000 -P ../phase.txt
+done
+
+root -b -q '../GCF_Ar_LUND.C(100,"ar_6gev")'
