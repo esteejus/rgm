19fea414564da94347b06dfe96db51fbdc5daf4c
Erin Seroka <erinseroka@gwu.edu>
2022-09-26 13:41:34 -0400

    Adding neutron efficiency scripts

--- Monitoring/neff_d_pcdn.cpp
+++ Monitoring/neff_d_pcdn.cpp
@@ -95,7 +95,7 @@ int main(int argc, char ** argv)
   auto config_c12=chain.GetC12Reader();
   chain.SetReaderTags({0});
   const std::unique_ptr<clas12::clas12reader>& c12=chain.C12ref();
-  //chain.db()->turnOffQADB();                 
+  chain.db()->turnOffQADB();                 
 
         
   /////////////////////////////////////
