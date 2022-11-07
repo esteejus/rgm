0425e5ebc1f687dfa31c995e99987617b41ca588
Erin Seroka <erinseroka@gwu.edu>
2022-11-07 17:57:39 -0500

    Now showing missing mass (with fits) before and after background subtraction

--- NeutronEfficiency/neff_d_pfdn.cpp
+++ NeutronEfficiency/neff_d_pfdn.cpp
@@ -52,7 +52,7 @@ Double_t signal(Double_t *x, Double_t *par);
 Double_t mmiss_signal_gauss(Double_t *x, Double_t *par);
 //double * hist_projections_gauss(TCanvas * can, TH2D * hist2d, int num_hist);
 double * hist_projections(TCanvas * can, TH2D * hist2d, int num_hist);
-double * hist_projections_backsub(TCanvas * can, TH2D * hist2d, int num_hist);
+double * hist_projections_backsub(TCanvas * can, TH2D * hist2d, int num_hist, bool subtract_bk);
 
 
 
@@ -657,9 +657,17 @@ int main(int argc, char ** argv)
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 */
+
+
+  // mmiss in pmiss bins
+  myCanvas->Divide(5,4);
+  hist_projections_backsub(myCanvas, h_mmiss_pmissCAND, neff_pbins, 0);
+  myCanvas->Print(fileName,"pdf");
+  myCanvas->Clear();
+
   // mmiss in pmiss bins with background subtracted
   myCanvas->Divide(5,4);
-  double *Ssub_pCAND = hist_projections_backsub(myCanvas, h_mmiss_pmissCAND,neff_pbins);
+  double *Ssub_pCAND = hist_projections_backsub(myCanvas, h_mmiss_pmissCAND, neff_pbins, 1);
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 
@@ -704,9 +712,17 @@ int main(int argc, char ** argv)
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 */
+
+
+  // mmiss by theta bins
+  myCanvas->Divide(5,4);
+  hist_projections_backsub(myCanvas, h_mmiss_thetaCAND, neff_tbins, false);
+  myCanvas->Print(fileName,"pdf");
+  myCanvas->Clear();
+
   // mmiss by theta bins - background subtracted
   myCanvas->Divide(5,4);
-  double *Ssub_tCAND = hist_projections_backsub(myCanvas, h_mmiss_thetaCAND, neff_tbins);
+  double *Ssub_tCAND = hist_projections_backsub(myCanvas, h_mmiss_thetaCAND, neff_tbins, true);
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
   
@@ -855,10 +871,17 @@ int main(int argc, char ** argv)
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 */
+
+
+  // mmiss in pmiss bins
+  myCanvas->Divide(5,4);
+  hist_projections_backsub(myCanvas, h_mmiss_pmissDET, neff_pbins, false);
+  myCanvas->Print(fileName,"pdf");
+  myCanvas->Clear();
  
   // mmiss in pmiss bins with background subtracted
   myCanvas->Divide(5,4);
-  double * Ssub_pDET = hist_projections_backsub(myCanvas, h_mmiss_pmissDET, neff_pbins);
+  double * Ssub_pDET = hist_projections_backsub(myCanvas, h_mmiss_pmissDET, neff_pbins, true);
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 /* 
@@ -894,7 +917,12 @@ int main(int argc, char ** argv)
 */
 
   myCanvas->Divide(5,4);
-  double * Ssub_tDET = hist_projections_backsub(myCanvas, h_mmiss_thetaDET, neff_tbins);
+  hist_projections_backsub(myCanvas, h_mmiss_thetaDET, neff_tbins, false);
+  myCanvas->Print(fileName,"pdf");
+  myCanvas->Clear();
+
+  myCanvas->Divide(5,4);
+  double * Ssub_tDET = hist_projections_backsub(myCanvas, h_mmiss_thetaDET, neff_tbins, true);
   myCanvas->Print(fileName,"pdf");
   myCanvas->Clear();
 /*
@@ -1072,16 +1100,18 @@ double get_pin_mmiss(TVector3 p_b, TVector3 p_e, TVector3 ppi){
 
 
 
-// make histogram projections, fit to 2 Gaussians, subtract background
-double * hist_projections_backsub(TCanvas * can, TH2D * hist2d, int num_hist)
+// input: 2d histogram of missing mass vs either momentum or theta
+// output: create 1d histograms in intervals of momentum or theta, fit to 2 Gaussians (optional subtract background)
+// output: interval of missing mass peak from Mlow to Mhigh
+// if subtract_bk = False, the missing mass histograms will be fit to the sum of 2 Gaussians (signal + background)
+// if subtract_bk = True, the background fit will be subtracted from the missing mass histogram, and the signal fit displayed
+double * hist_projections_backsub(TCanvas * can, TH2D * hist2d, int num_hist, bool subtract_bk)
 {
   double p_start_val[num_hist];
   double x_min = hist2d->GetXaxis()->GetXmin();
   double x_max = hist2d->GetXaxis()->GetXmax();
-  //double y_min = hist2d->GetYaxis()->GetXmin();
-  //double y_max = hist2d->GetYaxis()->GetXmin();
   double dp = (x_max-x_min)/num_hist;
-  double * Ssub = new double[num_hist];
+  double * S = new double[num_hist];
   // plot and fit each graph
   for (int i=0; i<num_hist; i++)
   {
@@ -1091,44 +1121,53 @@ double * hist_projections_backsub(TCanvas * can, TH2D * hist2d, int num_hist)
     // make projection for x interval
     can->cd(i+1);
     TH1D * proj = hist2d->ProjectionY("",bin1,bin2,"d");
-    proj->Draw();
-    // fit histogram
+    //proj->Draw();
+    // fit histogram to Gaussian (signal) + Gaussian (background)
     TF1 * cfit = new TF1("cfit",mmiss_signal_gauss,Mdisp_lo,Mdisp_hi,6);
     cfit->SetLineColor(kMagenta);
     cfit->SetParameters(10000,0.94,0.05,1200,1.3,0.1);
     cfit->SetParLimits(0,0,1000000);
-    cfit->SetParLimits(1,0.9,1.0);
+    cfit->SetParLimits(1,0.92,1.01);
     cfit->SetParLimits(2,0.02,0.1);
     cfit->SetParLimits(3,0,1000000);
-    cfit->SetParLimits(4,1.2,1.5);
-    cfit->SetParLimits(5,0,0.3);
-    proj->Fit("cfit","Q");  
-    //cfit->Draw("same");
+    cfit->SetParLimits(4,1.1,1.8);
+    cfit->SetParLimits(5,0.05,0.3);
+    proj->Fit("cfit","QN");
+    //if (subtract_bk)  {cfit->Draw("same");}
     // separate signal/background
+    // fit background
     Double_t par[6];
     cfit->GetParameters(par);
+std::cout << par[3] << '\t' << par[4] << '\t' << par[5] << endl;
     TF1 * bkfit = new TF1("backfit",signal,Mdisp_lo,Mdisp_hi,3);
-    bkfit->SetLineColor(kBlue);
+    //bkfit->SetLineColor(kBlue);
     bkfit->SetParameters(&par[3]);
-    //bkfit->Draw("same");
+    //if (subtract_bk)  {bkfit->Draw("same");}
+    // fit signal
     TF1 * sgfit = new TF1("sgfit",signal,Mdisp_lo,Mdisp_hi,3);
-    sgfit->SetLineColor(kGreen);
+    //sgfit->SetLineColor(kGreen);
     sgfit->SetParameters(par);
-    //sgfit->Draw("same");
-    // background subtraction
-    proj->Add(bkfit,-1);
+    // background subtraction!
+    if (subtract_bk)  {proj->Add(bkfit,-1);}
+    // draw stuff
     proj->Draw();
+    sgfit->SetLineColor(kRed);  sgfit->Draw("same");
+    if (!subtract_bk)
+    {
+      cfit->SetLineColor(kGreen);  cfit->Draw("same");
+      bkfit->SetLineColor(kBlue);  bkfit->Draw("same");
+    }
     // find background-subtracted signal
-    //Ssub[i] = sgfit->Integral(Mlow,Mhigh);
-    Ssub[i] = proj->Integral(proj->GetXaxis()->FindBin(Mlow),proj->GetXaxis()->FindBin(Mhigh));
+    S[i] = proj->Integral(proj->GetXaxis()->FindBin(Mlow),proj->GetXaxis()->FindBin(Mhigh));
     //std::cout << "integral = " << proj->Integral(Mlow,Mhigh) << '\n';
     //std::cout << "integral and error = " << proj->IntegralAndError(Mlow,Mhigh) << '\n';
   }
-  return Ssub;
+  return S;
 }
 
 
 // make histogram projections, fit to 2 Gaussians
+
 double * hist_projections(TCanvas * can, TH2D * hist2d, int num_hist)
 {
   double p_start_val[num_hist];
