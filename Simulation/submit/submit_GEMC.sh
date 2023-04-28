685516c7ab27920c3c64ffce89e3609b4aab62bb
Erin Seroka <erinseroka@gwu.edu>
2023-04-28 15:07:25 -0400

    Updated software environment

--- Simulation/submit/submit_GEMC.sh
+++ Simulation/submit/submit_GEMC.sh
@@ -8,19 +8,23 @@
 #SBATCH --time=20:00:00                                                                                               
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out                                                                           
 #SBATCH --error=//farm_out/%u/%x-%j-%N.err                                                                           
-#SBATCH --array=1-1                                                                                                     
+#SBATCH --array=1-1000                                                                                                
 
-NEVENTS=10
+NEVENTS=10000
 TORUS=-1.0
-FILE_PREFIX=gcf_d #Change file prefix for your simulation
+FILE_PREFIX=qe_c_6gev #Change file prefix for your simulation
 
-GCARD=./rgm.gcard
-YAML=./rgm_mc.yaml
+GCARD=rgm.gcard
+YAML=rgm_mc.yaml
 
 source /etc/profile.d/modules.sh
 source /group/clas12/packages/setup.sh
+module load clas12/dev # new
+module switch gemc/5.1 # new
 module load sqlite/dev
-module load clas12/pro
+#module load clas12/pro
+# I had to switch to the software versions Justin uses to avoid a seg fault
 
-gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ../lundfiles/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ../mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
+gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=10000 -INPUT_GEN_FILE="lund, ../lundfiles/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ../mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
+echo FINISHED GEMC
 recon-util -y $YAML -i ../mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o ../reconhipo/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
