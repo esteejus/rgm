0a7635a3de408a34736592183a5e0efa87e4e786
esteejus <justin.estee@gmail.com>
2023-08-25 17:16:51 -0400

    Forgot to add ! in if statment. Fixed bug.

--- libraries/clas12ana/clas12ana.cpp
+++ libraries/clas12ana/clas12ana.cpp
@@ -78,12 +78,12 @@ void clas12ana::Run(const std::unique_ptr<clas12::clas12reader>& c12)
 
       std::for_each(electrons_det.begin(),electrons_det.end(),[this](auto el)
 		    {
-		      if((el->che(HTCC)->getNphe() <= 2)        || //Photo electron min cut
+		      if(!((el->che(HTCC)->getNphe() <= 2)        || //Photo electron min cut
 			 (!checkEcalSFCuts(el) && f_ecalSFCuts) || //ECAL SF cuts
 			 (!checkEcalPCuts(el) && f_ecalPCuts)   || //ECAL SF cuts
 			 (!EcalEdgeCuts(el) && f_ecalEdgeCuts)  || //ECAL edge cuts
 			 (!checkVertex(el)  && f_vertexCuts)    || //Vertex cut
-			 (!DCEdgeCuts(el)   && f_DCEdgeCuts) )     //DC edge cut
+			   (!DCEdgeCuts(el)   && f_DCEdgeCuts)) )     //DC edge cut
 			setByPid(el);
 		    });
 
